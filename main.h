d59ae45ecec0d40ccc303559d4180b815caa890f
binbashz <5732@holbertonstudnets.com>
2023-04-24 12:26:49 -0700

    add new header

--- main.h
+++ main.h
@@ -3,9 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> /* header for strtok */
-#include <errno.h> /* for perror alert */
+#include <errno.h> /* header perror alert */
+#include <sys/wait.h> /* header for wait */
 #include <unistd.h>
-#include <sys/wait.h>
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
